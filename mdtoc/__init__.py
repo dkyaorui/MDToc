d7d965868eac899651b8d5ce1b1766d32018f246
dkyaorui <dkyaorui@163.com>
2019-05-18 21:44:40 +0800

    完成目录结构获取

--- mdtoc/__init__.py
+++ mdtoc/__init__.py
@@ -1,19 +1,49 @@
-from mdtoc.model import MdToc
+from mdtoc.model import MdToc, HeadNode
 from mdtoc.config import config
 import click
 
 
 @click.command()
 @click.option("--file", default=None, help="The path of target *.md file.")
-def start(file):
+def start(file: str):
+    """
+    开始处理 markdown 文件
+    Start process the markdown file
+    :param file: 文件完整路径或相对路径 the path of markdown file
+    :return: 处理结果 the result of process
+    """
     if file is None:
         click.echo("Please use --help option read doc.")
     else:
         toc = MdToc(config)
-        file_path, file_name = toc.get_file_path_and_filename(file)
+        # 存放 H1 节点信息
+        head_list = list()
+        now_node = None
+        file_path, file_name = toc.get_filepath_and_filename(file)
         print(file_path, file_name)
         with open(file, "r") as f:
             for line in f.readlines():
                 res = toc.process_line(line)
                 if res:
-                    print(res)
+                    # 如果匹配到标题
+                    tag, content = res
+                    level = toc.get_level(tag)
+                    node = HeadNode(level, content)
+                    if now_node is None:
+                        # 初始化第一个节点为 now_node
+                        now_node = node
+                        head_list.append(now_node)
+                    else:
+                        if node.level == -1:
+                            # 将 H1 节点放入 head_list
+                            head_list.append(node)
+                        else:
+                            if node.level < now_node.level:
+                                # 如果节点等级小于当前节点，将其变为孩子
+                                HeadNode.add_child(node, now_node)
+                            else:
+                                father = HeadNode.get_father_node(node, now_node)
+                                HeadNode.add_child(node, father)
+                        now_node = node
+        for i in head_list:
+            HeadNode.print_node_tree(i)
--- mdtoc/model.py
+++ mdtoc/model.py
@@ -4,19 +4,78 @@ import regex as re
 class MdToc(object):
 
     def __init__(self, config: dict):
-        self.HEAD = config.get("HEAD")
+        self.HEAD = config["pattern"].get("HEAD")
+        self.head_info = config["constant"].get("HEAD")
 
     @staticmethod
-    def get_file_path_and_filename(file_info: str):
+    def get_filepath_and_filename(file_info: str):
+        """获取文本路径信息"""
         file_info_data = file_info.split('/')
         file_name = file_info_data[-1]
         file_path = "/".join(file_info_data[:-1])
         return file_path, file_name
 
     def process_line(self, line: str):
+        """处理读取的一行内容"""
         for h, v in self.HEAD.items():
             m = re.search(v, line)
             if m is None:
                 continue
-            return m.group(0)
+            return m.group(0).split(" ", 1)
+        return []
+
+    def get_level(self, tag: str):
+        for h, val in self.head_info.items():
+            if tag == val["content"]:
+                return val["level"]
+        return None
+
+
+class HeadNode(object):
+    def __init__(self, level: int, content: str, father=None):
+        """
+        H1-H6 标签节点
+        :param level: 节点的登记，1=》H1····
+        :param content: 标题内容
+        :return: None
+        """
+        self.level = level
+        self.content = content
+        self.father = father
+        self.children = list()
+
+    @staticmethod
+    def add_child(node, target_node):
+        """
+        在 self.children 中添加子节点
+        :param node: Node，标签节点
+        :param target_node: 父亲节点
+        :return:
+        """
+        if isinstance(node, HeadNode):
+            node.father = target_node
+            target_node.children.append(node)
+            return True
         return False
+
+    @staticmethod
+    def get_father_node(node, target_node):
+        """
+        找到 node 的父亲
+        :param node: 没有父亲的节点
+        :param target_node: 目标节点家族
+        :return:
+        """
+        if node.level == target_node.level:
+            return target_node.father
+        elif node.level < target_node.level:
+            return target_node
+        else:
+            return HeadNode.get_father_node(node, target_node.father)
+
+    @staticmethod
+    def print_node_tree(node):
+        """打印当前节点下的所有子节点"""
+        print("-" * abs(node.level), node.content)
+        for i in node.children:
+            HeadNode.print_node_tree(i)
